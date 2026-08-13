2bf00557a9fc1308ca168f572de4a052b778f563
Jolo Somera <somerajolo15@gmail.com>
2026-08-13 20:29:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import com.example.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            UnscrambleTheme {
+                GameScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "UNSCRAMBLE",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = "TAC",
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "Unscramble the word!"
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = {
+                Text("Enter your answer")
+            }
+        )
+
+        Button(
+            onClick = { }
+        ) {
+            Text("SUBMIT")
+        }
+
+        Text(
+            text = "Score: 0"
+        )
+    }
+}
